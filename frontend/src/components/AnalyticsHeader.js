0c0e7bbc5e4120b1587b5add1566ba6716258bb8
angeloxenakis <angeloxenakis@gmail.com>
2021-09-08 15:07:00 -0500

    rough styling for list view and analytics

---
+++ frontend/src/components/AnalyticsHeader.js
@@ -0,0 +1,12 @@
+import React from "react"
+import "../styles/AnalyticsHeader.css"
+
+export const AnalyticsHeader = () => {
+    return(
+        <>
+            <div className="analytics-header">
+                <h2>Analytics</h2>
+            </div>
+        </>
+    )
+}
