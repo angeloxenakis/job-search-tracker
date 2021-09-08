dcd4500742e50adf66669a82f2303364f1d1f79b
angeloxenakis <angeloxenakis@gmail.com>
2021-09-08 15:49:42 -0500

    search works and sorting is halfway done

--- frontend/src/components/OppHeader.js
+++ frontend/src/components/OppHeader.js
@@ -1,20 +1,37 @@
 import React from "react"
 import "../styles/OppHeader.css"
 
-export const OppHeader = ({ toggleView }) => {
+export const OppHeader = ({ toggleView, filterOpps, sortOpps }) => {
+
+    const handleSearchInput = (e) => {
+        filterOpps(e.target.value)
+    }
+
+    const handleSortSelection = (e) => {
+        sortOpps(e.target.value)
+    }
+
     return(
         <div>
             <div className="opp-header">
                 <h2>Opportunities</h2>
                 <div className="opp-header-tools">
+                    <div className="opp-header-search">
+                        Search: <input type="text" placeholder="Title, Company, or Location" onChange={(e) => handleSearchInput(e)}></input>
+                    </div>
                     <div className="opp-header-sort">
-                        <h4>Sort by:</h4>
-                        <select>
-                            <option>Date Applied</option>
-                            <option>Date Applied</option>
+                        Sort by:
+                        <select onChange={(e) => handleSortSelection(e)}>
+                            <option value="job_title">Job Title</option>
+                            <option value="company">Company</option>
+                            <option value="salary">Salary</option>
+                            <option value="location">Location</option>
+                            <option value="interest_level">Interest</option>
+                            <option value="date_applied">Date Applied</option>
+                            <option value="last_contact">Date Applied</option>
                         </select>
                     </div>
-                    <h4>View: <span onClick={() => toggleView("tile")}>Tile</span> | <span onClick={() => toggleView("list")}>List</span></h4>
+                    View: <span onClick={() => toggleView("tile")}>Tile</span> | <span onClick={() => toggleView("list")}>List</span>
                 </div>
             </div>
         </div>
