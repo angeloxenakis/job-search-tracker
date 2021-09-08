ee15450ba5455117d6e3d4877cdc9c88083c0eb3
angeloxenakis <angeloxenakis@gmail.com>
2021-09-07 20:40:29 -0500

    modal works with fetch and state

--- frontend/src/components/OppModal.js
+++ frontend/src/components/OppModal.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react"
 
-export const OppModal = ({modalClass, toggleModal}) => {
+export const OppModal = ({modalClass, toggleModal, addOpportunity}) => {
 
     const [ formValues, setFormValues ] = useState({
         job_title: "",
@@ -8,19 +8,30 @@ export const OppModal = ({modalClass, toggleModal}) => {
         description: "",
         salary: "",
         location: "",
-        interest: "",
+        interest_level: "",
         data_applied: "",
         last_contact: ""
     })
 
     const handleFormInput = (e) => {
-        console.log(e.target.name)
         formValues[e.target.name] = e.target.value
-        console.log(formValues)
     }
 
     const handleSubmit = (e) => {
         e.preventDefault()
+
+        fetch("http://localhost:3000/opportunities", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(formValues)
+        })
+        .then(res => res.json())
+        .then(newOpp => addOpportunity(newOpp))
+
+        e.target.reset()
+        toggleModal()
     }
 
     return(
@@ -34,7 +45,7 @@ export const OppModal = ({modalClass, toggleModal}) => {
                         Description: <input type="text" name="description" onChange={(e) => handleFormInput(e)}/><br/><br/>
                         Salary: <input type="text" name="salary" onChange={(e) => handleFormInput(e)}/><br/><br/>
                         Location: <input type="text" name="location" onChange={(e) => handleFormInput(e)}/><br/><br/>
-                        Interest: <input type="text" name="interest" onChange={(e) => handleFormInput(e)}/><br/><br/>
+                        Interest: <input type="text" name="interest_level" onChange={(e) => handleFormInput(e)}/><br/><br/>
                         Date Applied: <input type="text" name="date_applied" onChange={(e) => handleFormInput(e)}/><br/><br/>
                         Last Contact: <input type="text" name="last_contact" onChange={(e) => handleFormInput(e)}/><br/><br/>
                         <input type="submit" />
